b0d5f6c1e1e1a4d0c0f6db9eb0b4cea3977ab262
DM-09 <112751504+DM-09@users.noreply.github.com>
2022-09-03 17:32:06 +0900

    Update inputservice.py

--- inputservice.py
+++ inputservice.py
@@ -8,4 +8,4 @@ def input_service(sentence, types='str', error_msg='error'):
         print(error_msg)
       
   
-  # input_service: 입력받은 문자가 원하는 타입(문자열, 정수 등)이 아니라면 메시지를 출력하고 워하는 타입이 나올 때까지 묻는 함수
+  # input_service 설명: 입력받은 문자가 원하는 타입(문자열, 정수 등)이 아니라면 메시지를 출력하고 워하는 타입이 나올 때까지 묻는 함수
