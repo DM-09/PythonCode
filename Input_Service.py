01617ec91f03b8956a0964eee29eb0024e843a9d
DM <112751504+DM-09@users.noreply.github.com>
2022-09-04 11:32:46 +0900

    Update Input_Service.py

--- Input_Service.py
+++ Input_Service.py
@@ -16,12 +16,12 @@ def input_service(sentence, types='str', error_msg='error'):
   Description: A function that outputs a message and prompts you until the type 
   you want is not the type you want (string, integer, etc.)
 
-  Argument: sentence = str, types = class type, error_msg = str
+  Argument: sentence = str, types = class type(str), error_msg = str
 
   설명: 입력받은 문자가 원하는 타입(문자열, 정수 등)이 아니라면 메시지를 출력하고 
   원하는 타입이 나올 때까지 묻는 함수
 
-  인자: sentence=문자열, types=클래스 타입, error_msg = 문자열
+  인자: sentence=문자열, types=클래스 타입(문자열), error_msg = 문자열
   '''
 
 
