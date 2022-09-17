e6bf0bac212dcbeb26518028381beb531da6f76b
DM <112751504+DM-09@users.noreply.github.com>
2022-09-17 16:56:26 +0900

    Update Type_Service.py

--- function/Type_Service.py
+++ function/Type_Service.py
@@ -9,4 +9,11 @@ def type_service(word, types='str'):
             return 'false'
             
   '''
+  Description:
+  
+  Argument: word = str, types = class type(str)
+  
+  설명: 입력 받은 단어가 원하는 타입(문자열, 정수 등)이라면 'true'를 반환, 아니면 'false'를 반환 한다.
+  
+  인자: word=문자열, types=클래스 타입(문자열),
   '''
