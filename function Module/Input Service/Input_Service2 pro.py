14c41984ad110b84ee4de253348807d67f14a6ed
DM <112751504+DM-09@users.noreply.github.com>
2022-09-28 08:47:56 +0900

    Update Input_Service2 pro.py

--- function Module/Input Service/Input_Service2 pro.py
+++ function Module/Input Service/Input_Service2 pro.py
@@ -21,4 +21,5 @@ def input_service2(sentence, error_msg='error', max_num='2', types='int'):
 
 '''
 설명: 최댓값보다 같거나 작으면 설정한 타입으로 바꿔줌
+인자: sentence = 문자열(물어볼 말), error_msg = 문자열(에러 메시지), max_num = 문자열(최댓값), types: 문자열(타입)
 '''
