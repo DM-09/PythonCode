b930d6655019774305e01145356b57330ccb441e
DM <112751504+DM-09@users.noreply.github.com>
2023-01-22 13:24:25 +0900

    Create text to list.py

---
+++ ShortCodes/text to list.py
@@ -0,0 +1,12 @@
+print(input().split())
+
+'''
+When you need to put multiple strings in the list, this code switches to the list at once and prints it out.
+(Collaborator: blank)
+
+많은 문자열을 리스트에 넣을 때 이 코드로 한 번에 리스트화 시켜줄 수 있습니다.
+(구분자: 공백)
+
+example: hello word ➡ ['hello', 'word']
+예: 안녕 세상 ➡ ['안녕', '세상']
+'''
