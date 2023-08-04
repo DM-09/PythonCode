13a883f9c984e8865c0b04bdfa20aad0740dea8c
DM <happydm09@naver.com>
2023-08-04 18:51:50 +0900

    Update 이분탐색.py

--- Baekjoon/이분탐색.py
+++ Baekjoon/이분탐색.py
@@ -9,4 +9,6 @@ def Binary_Search(target, data):
         else: end = mid - 1
     return -1
 
-print(Binary_Search(10, [6, 3, 2, 10, -10]))
+print(Binary_Search(10, [6, 3, 2, 10, -10])) # 예시
+# Target: 찾을 요소, data: List
+# 찾을 요소가 있으면 그 요소의 인덱스를(정렬된 List에서의) 없으면 -1을 반환한다.
