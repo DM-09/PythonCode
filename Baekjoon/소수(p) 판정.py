9d00aec6879ed26aae2dfb5f37c191d4fb0768a8
DM <happydm09@naver.com>
2023-09-02 00:30:08 +0900

    Update 소수(p) 판정.py

--- Baekjoon/소수(p) 판정.py
+++ Baekjoon/소수(p) 판정.py
@@ -5,3 +5,15 @@ def isPrime(n):
     if n == 2: return True
     if math.factorial(n - 1) % n == n - 1: return True
     return False
+# 윌슨의 정리
+
+def isPrime(n):
+    import math
+
+    if n == 1: return False
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0: return False
+    return True
+
+# 제곱근 까지 브루트포스로 
