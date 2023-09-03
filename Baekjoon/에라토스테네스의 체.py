c9459f24519f3eac5a27b95615f40232181024a7
DM <happydm09@naver.com>
2023-09-03 10:36:03 +0900

    Create 에라토스테네스의 체.py

---
+++ Baekjoon/에라토스테네스의 체.py
@@ -0,0 +1,10 @@
+def SOE(MAX, MIN=1):
+    box, l = [], [i + 1 for i in range(MAX)]
+
+    for i in range(MAX):
+        c = l[i]
+        if c != 1 and c != 0:
+            for j in range(2, MAX // c + 1):
+                l[c * j - 1] = 0
+            if c >= MIN: box.append(c)
+    return box
